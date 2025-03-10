29a3e7babcb0a8127de5dfd71234636c18b8b0df
chuvitvang <152381244+chuvitvang@users.noreply.github.com>
2025-03-10 22:34:36 +0700

    Update baitap1.py

--- baitap1.py
+++ baitap1.py
@@ -1,19 +1,23 @@
 import numpy as np
 
+# Định nghĩa ma trận hướng D8 toàn cục
+D8_CODES = np.array([[32, 64, 128], [16, 0, 1], [8, 4, 2]])
+
 def read_asc(file_path):
     """ Đọc file DEM .asc và trả về ma trận độ cao """
     with open(file_path, 'r') as f:
         lines = f.readlines()
     
     metadata = [float(lines[i].split()[1]) for i in range(6)]
-    ncols, nrows, xllcorner, yllcorner, cellsize, nodata = map(int, metadata[:2]) + metadata[2:]
+    ncols, nrows = map(int, metadata[:2])
+    xllcorner, yllcorner, cellsize, nodata = metadata[2:]
+    
     data = np.loadtxt(lines[6:], dtype=float)
     data[data == nodata] = np.nan
     return data, ncols, nrows, cellsize, nodata
 
 def compute_flow_direction(elevation):
     """ Tính hướng dòng chảy D8 """
-    D8_CODES = np.array([[32, 64, 128], [16, 0, 1], [8, 4, 2]])
     flow_direction = np.zeros_like(elevation, dtype=int)
     
     for i in range(1, elevation.shape[0] - 1):
@@ -35,13 +39,13 @@ def compute_flow_accumulation(elevation, flow_direction):
             for di, dj in [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]:
                 ni, nj = i + di, j + dj
                 if 0 <= ni < elevation.shape[0] and 0 <= nj < elevation.shape[1]:
-                    if flow_direction[ni, nj] == D8_CODES[di + 1, dj + 1]:
+                    if flow_direction[ni, nj] == D8_CODES[di + 1, dj + 1]:  # D8_CODES giờ đã được định nghĩa
                         flow_accumulation[i, j] += 1
     
     return flow_accumulation
 
 # Đọc dữ liệu DEM
-dem_file = "dem.asc"
+dem_file = "C:/Users/Admin/Downloads/bt1.asc"
 elevation, ncols, nrows, cellsize, nodata_value = read_asc(dem_file)
 
 # Tính toán hướng dòng chảy và tích lũy dòng chảy
